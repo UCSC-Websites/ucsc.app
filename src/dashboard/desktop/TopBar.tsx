632c9840d772ee43ebeee8c9902754a218c98349
iconsumeplutonium <uzymc242@outlook.com>
2025-12-23 19:07:01 -0800

    fix weird production issue

--- src/dashboard/desktop/TopBar.tsx
+++ src/dashboard/desktop/TopBar.tsx
@@ -1,4 +1,4 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router';
 import './TopBar.css';
 import TopBarButton from '../../TopBarButton';
 import ThemeToggle from '../../ThemeChanger';
