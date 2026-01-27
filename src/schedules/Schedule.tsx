ee8ddf26b36969a30bed389dc165ca710fd76732
iconsumeplutonium <uzymc242@outlook.com>
2026-01-27 03:41:47 -0800

    shut up and build already

--- src/schedules/Schedule.tsx
+++ src/schedules/Schedule.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { MapContext } from "./MapContext";
 import { TimeBlock } from "../types";
 import ScheduleTimeBlock from "./components/ScheduleTimeBlock";
